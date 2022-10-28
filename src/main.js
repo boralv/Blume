b4812a3b479da771d60fdce527dce69ac5e6261c
bora <36164764+boralv@users.noreply.github.com>
2022-10-28 14:48:12 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import './style.css'
-
 createApp(App).mount('#app')
+
+$(function() {
+    $(".hint").each(function() {
+        var str = $( this ).html();
+        if(!str.includes('$')){
+            $( this ).html( "$" + str );
+        }
+    });
+});
