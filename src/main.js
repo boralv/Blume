2987a52845c45b496d05172852efc544ddd48f96
bora <36164764+boralv@users.noreply.github.com>
2022-10-28 17:47:32 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -11,3 +11,14 @@ $(function() {
         }
     });
 });
+
+$("cards").onmousemove = e => {
+    for(const card of document.getElementsByClassName("flower")) {
+        const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+
+        card.style.setProperty("--mouse-x", `${x}px`);
+        card.style.setProperty("--mouse-y", `${y}px`);
+    };
+}
