0b13c46f93231b04bfb9f71fa7653d0fd53c7af4
bora <36164764+boralv@users.noreply.github.com>
2022-10-29 17:21:54 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,61 @@
-<!-- TODO: responsive refinement, dropdown sort, filter logic, clean up code -->
+<!-- TODO: responsive refinement, dropdown sort, filter logic-->
 <template>
-  <div class="main">
-    <h1 class="main-header">Blume</h1>
-    <div class="wrapper">
-      <div class="filters">
-        <div class="empty filter"></div>
-        <div class="categories-title filter" @click="rotate()">
-          <span class="dropdown-title lighter">Categories</span>
-          <span class="dropdown-arrow" :class="[isHidden ? 'down' : 'up']"></span>
+  <h1 class="main-header">Blume</h1>
+  <div class="wrapper">
+    <div class="filters">
+      <div class="empty filter"></div>
+      <div class="categories-title filter" @click="rotate()">
+        <span class="dropdown-title lighter">Categories</span>
+        <span class="dropdown-arrow" :class="[isHidden ? 'down' : 'up']"></span>
+      </div>
+      <Transition>
+        <div class="categories-list filter" v-if="!isHidden">
+          <label v-for="(t, index) in types" class="container">
+            <input type="checkbox" :key="t.name" :id="`cb${index}`" :value="t.name.toLowerCase().split(' ').join('')" />
+            <span class="checkmark"></span>
+            {{ t.name }}
+          </label>
         </div>
-        <Transition>
-          <div class="categories-list filter" v-if="!isHidden">
-            <label v-for="(t, index) in types" class="container">
-              <input type="checkbox" :key="t.name" :id="`cb${index}`"
-                :value="t.name.toLowerCase().split(' ').join('')" />
-              <span class="checkmark"></span>
-              {{ t.name }}
-            </label>
-          </div>
-        </Transition>
-        <div class="price-range filter">
-          <span class="price-title lighter">Price Range</span>
-          <div class="price-slider">
-            <input data-role="doubleslider" class="ultra-thin cycle-marker" data-min="1" data-max="10"
-              data-hint-position-min="bottom" data-hint-position-max="bottom" data-hint-always="true"
-              data-cls-complete="bg-dark" data-cls-hint="bg-light fg-dark text-bold"
-              data-on-move="$('.hint').each(function() {var str = $(this).html();if(!str.includes('$')){$(this).html('$ ' + str);}});" />
-          </div>
+      </Transition>
+      <div class="price-range filter">
+        <span class="price-title lighter">Price Range</span>
+        <div class="price-slider">
+          <input data-role="doubleslider" class="ultra-thin cycle-marker" data-min="1" data-max="10"
+            data-hint-position-min="bottom" data-hint-position-max="bottom" data-hint-always="true"
+            data-cls-complete="bg-dark" data-cls-hint="bg-light fg-dark text-bold"
+            data-on-move="$('.hint').each(function() {var str = $(this).html();if(!str.includes('$')){$(this).html('$ ' + str);}});" />
         </div>
       </div>
-      <div class="content">
-        <div class="flowers-title">
-          <h2 class="flowers-header">New Arrivals</h2>
-          <div class="flowers-sort" @click="drop()">
-            <span class="dropdown-title lighter">Sort by Price</span>
-            <span class="dropdown-arrow" :class="[isDropped ? 'up' : 'down']"></span>
-          </div>
+    </div>
+    <div class="content">
+      <div class="flowers-title">
+        <h2 class="flowers-header">New Arrivals</h2>
+        <div class="flowers-sort" @click="drop()">
+          <span class="dropdown-title lighter">Sort by Price</span>
+          <span class="dropdown-arrow" :class="[isDropped ? 'up' : 'down']"></span>
         </div>
-        <div class="flowers-grid" id="cards">
-          <div v-for="blume in blumen" class="flower">
-            <div :key="blume.name" :id="`bl${blume.id}`" class="card-content">
-              <div class="card-info-wrapper">
-                <span class="flower-accent"></span>
-                <span class="flower-type">{{ blume.type }}</span>
-                <h3 class="flower-name">{{ blume.name }}</h3>
-              </div>
-              <div class="card-image">
-                <img :src="'/src/images/' + blume.picture" />
-              </div>
-              <div class="card-footer">
-                <span class="price-title">Price</span>
-                <span class="flower-price">$ {{ blume.price }}</span>
-              </div>
+      </div>
+      <div class="flowers-grid" id="cards">
+        <div v-for="blume in blumen" class="flower">
+          <div :key="blume.name" :id="`bl${blume.id}`" class="card-content">
+            <div class="card-info-wrapper">
+              <span class="flower-accent"></span>
+              <span class="flower-type">{{ blume.type }}</span>
+              <h3 class="flower-name">{{ blume.name }}</h3>
+            </div>
+            <div class="card-image">
+              <img :src="'/src/images/' + blume.picture" />
+            </div>
+            <div class="card-footer">
+              <span class="price-title">Price</span>
+              <span class="flower-price">$ {{ blume.price }}</span>
             </div>
           </div>
         </div>
       </div>
     </div>
-    <footer>Created by Boriss Dvornikovs</footer>
   </div>
+  <footer>Created by Boriss Dvornikovs</footer>
 </template>
 
 <script>
@@ -94,11 +91,11 @@ export default {
     height: 100vh;
     position: sticky;
     top: 0;
-    
+
     .empty {
       height: 2.8em;
     }
-    
+
     .filter {
       border-right: $hr;
       border-bottom: $hr;
@@ -114,6 +111,7 @@ export default {
 .categories-title {
   display: flex;
   align-items: center;
+  overflow: hidden;
   justify-content: space-between;
   padding-bottom: 0.5em;
   cursor: pointer;
@@ -135,26 +133,27 @@ export default {
     }
 
     .checkmark {
-      height: 0.9em;
-      width: 0.9em;
-      margin-right: 0.8em;
+      height: 1em;
+      width: 1em;
+      margin-right: 0.7em;
       border: 1px solid rgba(128, 128, 128, 0.3);
       border-radius: 4px;
       display: inline-block;
       place-content: center;
       transition: ease-in-out 120ms;
       position: relative;
+      top: 2px;
     }
 
     .checkmark:before {
       content: "";
       position: absolute;
-      top: 0.25em;
-      left: 0.25em;
-      width: 0.3em;
-      height: 0.3em;
+      top: 0.3em;
+      left: 0.3em;
+      width: 0.35em;
+      height: 0.35em;
       transform: scale(0);
-      border-radius: 1px;
+      border-radius: 2px;
       box-shadow: inset 1em 1em white;
       transition: 120ms transform ease-in-out;
     }
@@ -199,9 +198,10 @@ export default {
 
   .flowers-sort {
     cursor: pointer;
+
     .dropdown-title {
-    padding: 0 1em;
-}
+      padding: 0 1em;
+    }
   }
 }
 
@@ -216,13 +216,14 @@ export default {
   }
 
   div.itslit {
-    border: 3px outset rgba(124, 179, 66, 0.24);
+    border: 2px outset rgba(124, 179, 66, 0.4);
   }
 
   .flower {
+    display: block;
     background-color: rgba(255, 255, 255, 0.8);
-    border: 1px solid rgba(128, 128, 128, 0.24);
-    transition: border 100ms ease-in-out 0s;
+    border: 1px solid rgba(128, 128, 128, 0.16);
+    transition: border 120ms ease-in-out 0s;
     width: 284px;
     height: 335px;
     border-radius: 14px;
@@ -247,13 +248,13 @@ export default {
     }
 
     &::before {
-      background: radial-gradient(600px circle at var(--mouse-x) var(--mouse-y), rgba(148, 148, 148, 0.08),
+      background: radial-gradient(800px circle at var(--mouse-x) var(--mouse-y), rgba(101, 124, 100, 0.08),
           transparent 40%);
       z-index: 3;
     }
 
     &::after {
-      background: radial-gradient(400px circle at var(--mouse-x) var(--mouse-y), rgba(148, 148, 148, 0.4),
+      background: radial-gradient(600px circle at var(--mouse-x) var(--mouse-y), rgba(101, 124, 100, 0.4),
           transparent 40%);
       z-index: 1;
     }
@@ -275,10 +276,11 @@ export default {
 .flower-accent {
   position: absolute;
   top: 10px;
-  left: -26px;
-  width: 4px;
+  left: -27px;
+  width: 5px;
   height: 36px;
   background-color: var(--accent-color);
+  opacity: .9;
 }
 
 .card-content {
